e0b8bdf27c48635daa58b983488d9861679f6d06
Wobas <138794906+Wobas@users.noreply.github.com>
2025-03-18 14:57:32 +0300

    new functions and combining changes in layers

--- ngw_connect_versions.py
+++ ngw_connect_versions.py
@@ -3,7 +3,14 @@ import json
 import os
 import schedule
 import time
+import base64
+from datetime import datetime
 import pandas as pd
+import geopandas as gpd
+import shapely
+from shapely.geometry import Point, Polygon
+from shapely.wkt import loads
+from osgeo import ogr, osr
 
 
 class ErrorConnection(Exception):
@@ -54,8 +61,29 @@ class LinkParametersHolder:
                 bottom_layer_latest_version, bottom_layer_latest_epoch = bottom_layer_info['version'], bottom_layer_info['epoch'] 
                 bottom_layer_difference = self.__get_difference_between_versions(self.bottom_layer_id, 0, bottom_layer_latest_version, bottom_layer_latest_epoch)
 
-                print('getted json: ' , top_layer_difference, bottom_layer_difference)
+                ans = sorted(top_layer_difference['dif_list']+bottom_layer_difference['dif_list'], key=LinkParametersHolder.__get_time)
+                print('Объединненый список изменений в обоих слоях:', ans)
 
+                layers_path = os.path.join(self.tmp_files_path, 'layers')
+                file_name_and_path_top_layer = os.path.join(layers_path, f'layer_{self.top_layer_id}.gpkg')
+                layers = gpd.list_layers(file_name_and_path_top_layer)
+                top_layer_geometry = gpd.read_file(file_name_and_path_top_layer, layer=layers['name'][0])
+                print(top_layer_geometry.iterrows())
+
+                file_name_and_path_bottom_layer = os.path.join(layers_path, f'layer_{self.bottom_layer_id}.gpkg')
+                layers = gpd.list_layers(file_name_and_path_bottom_layer)
+                bottom_layer_geometry = gpd.read_file(file_name_and_path_bottom_layer, layer=layers['name'][0])
+
+                for item in ans:
+                    if item['layer_id'] == self.top_layer_id and 'geom' in item:
+                        wkb_data = base64.b64decode(item['geom'])
+                        current_geom = ogr.CreateGeometryFromWkb(wkb_data)
+                        print("Координаты точки:", current_geom)
+                        # for index, row in bottom_layer_geometry.iterrows():
+                            # geom = Polygon(row['geometry'])
+                            # print("Координаты вершин полигона: ", geom)
+                            # if geom.contains(current_geom):
+                            #     print("Попадос")
                 schedule.every(self.update_period_sec).seconds.do(lambda: self.__check_update())
 
                 while True:
@@ -74,12 +102,14 @@ class LinkParametersHolder:
         """
         req_top_layer = f'{self.ngw_host}/api/resource/{self.top_layer_id}/export?context=IFeatureLayer&format=GPKG&zipped=false'
         req_bottom_layer = f'{self.ngw_host}/api/resource/{self.bottom_layer_id}/export?context=IFeatureLayer&format=GPKG&zipped=false'
-        # переделать через with
         top_layer_info = requests.get(req_top_layer, stream = True, auth = (self.ngw_login, self.ngw_password))
         bottom_layer_info = requests.get(req_bottom_layer, stream = True, auth = (self.ngw_login, self.ngw_password))
 
-        if top_layer_info.status_code == 200 and bottom_layer_info.status_code == 200:
+        if top_layer_info.status_code == 200 and bottom_layer_info.status_code == 200:            
             layers_path = os.path.join(self.tmp_files_path, 'layers')
+            if not os.path.isdir(layers_path):
+                os.makedirs(layers_path)
+            
             file_name_and_path_top_layer = os.path.join(layers_path, f'layer_{self.top_layer_id}.gpkg')
             file_name_and_path_bottom_layer = os.path.join(layers_path, f'layer_{self.bottom_layer_id}.gpkg')
             
@@ -107,24 +137,20 @@ class LinkParametersHolder:
                 # close the connection anyway (due to stream = True)
                 top_layer_info.close()
                 bottom_layer_info.close()
-            if __debug__:
-                print(message)
-            return {'status':'error', 'message':message}
         
         elif top_layer_info.status_code != 200 and bottom_layer_info.status_code != 200:
-            message = f'Request errors: {top_layer_info.status_code} AND {bottom_layer_info.status_code}'
+            message = f'Request errors when getting GPKG files: {top_layer_info.status_code} AND {bottom_layer_info.status_code}'
         
         elif top_layer_info.status_code != 200:
-            message = f'Request error: {top_layer_info.status_code}'
+            message = f'Request error when getting top GPKG file: {top_layer_info.status_code}'
         
         elif bottom_layer_info.status_code != 200:
-            message = f'Request error: {bottom_layer_info.status_code}'
+            message = f'Request error when getting bottom GPKG file: {bottom_layer_info.status_code}'
 
         top_layer_info.close()
         bottom_layer_info.close()
-        if __debug__:
-            print(message)
-        return {'status':'error', 'message':message}
+
+        return LinkParametersHolder.__handle_error(message)
 
     def __save_file_with_cur_versions(self):
         """
@@ -166,24 +192,20 @@ class LinkParametersHolder:
                     print(f'Data file was successfully saved in {file_name_and_path}')
                 return {'status':'ok'}
             else:
-                message = f'Error when reading data file. Top layer status: {top_layer_version_info['message']}; Bottom layer status: {bottom_layer_version_info}'
-                if __debug__:
-                    print()
-                return {'status':'error', 'message':message}
+                message = f'Error when reading data from the server to save data file. Top layer status: {top_layer_version_info['message']}; Bottom layer status: {bottom_layer_version_info['message']}'
+
         except KeyError as e:
-            message = f"Error: {e}"
+            message = f"Error when saving data file: {e}"
         except FileNotFoundError:
-            message = f"Error: Directory {self.tmp_files_path} not exists."
+            message = f"Error when saving data file: Directory {self.tmp_files_path} not exists."
         except PermissionError:
-            message = f"Error: No rights to write to the directory {self.tmp_files_path}."
+            message = f"Error when saving data file: No rights to write to the directory {self.tmp_files_path}."
         except (TypeError, ValueError) as e:
             message = f"Error when serializing data in JSON: {e}"
         except Exception as e:
-            message = f"Unexpected error: {e}"
+            message = f"Unexpected error when saving data file: {e}"
 
-        if __debug__:
-            print(message)
-        return {'status':'error', 'message':message}
+        return LinkParametersHolder.__handle_error(message)
 
     def __check_update(self):
         """
@@ -195,8 +217,6 @@ class LinkParametersHolder:
         dict
             status key contains error or ok, if error then message key contains explanations, if ok then it contains nothing else
         """
-        # with open('data.json', 'r') as data_file:
-        #     data = json.load(data_file)
         latest_version_top_layer = self.__get_latest_version_and_epoch(self.top_layer_id)
         latest_version_bottom_layer = self.__get_latest_version_and_epoch(self.bottom_layer_id)
 
@@ -209,10 +229,24 @@ class LinkParametersHolder:
                 last_saved_version_bottom_layer, last_saved_epoch_bottom_layer = bottom_layer_info['version'], bottom_layer_info['epoch']
                 
                 if (last_saved_version_top_layer < latest_version_top_layer['version'] and last_saved_version_bottom_layer < latest_version_bottom_layer['version']):
-                    self.__get_difference_between_versions(self.top_layer_id, last_saved_version_top_layer, latest_version_top_layer['version'], last_saved_epoch_top_layer)
-                    self.__get_difference_between_versions(self.bottom_layer_id, last_saved_version_bottom_layer, latest_version_bottom_layer['version'], last_saved_epoch_bottom_layer)
-                    self.__save_file_with_cur_versions()
-                
+                    top_layer_dif_info = self.__get_difference_between_versions(self.top_layer_id, last_saved_version_top_layer, latest_version_top_layer['version'], last_saved_epoch_top_layer)
+                    bottom_layer_dif_info = self.__get_difference_between_versions(self.bottom_layer_id, last_saved_version_bottom_layer, latest_version_bottom_layer['version'], last_saved_epoch_bottom_layer)
+                    if (top_layer_dif_info['status'] == 'ok' and bottom_layer_dif_info['status'] == 'ok'):
+                        self.__save_file_with_cur_versions()
+                        ans = sorted(top_layer_dif_info['dif_list']+bottom_layer_dif_info['dif_list'], key=LinkParametersHolder.__get_time)
+                        
+                        layers_path = os.path.join(self.tmp_files_path, 'layers')
+                        file_name_and_path_top_layer = os.path.join(layers_path, f'layer_{self.top_layer_id}.gpkg')
+                        file_name_and_path_bottom_layer = os.path.join(layers_path, f'layer_{self.bottom_layer_id}.gpkg')
+                        # gdf = gpd.read_file(file_name_and_path_top_layer)
+
+                        layers = gpd.list_layers(file_name_and_path_top_layer)
+                        print("Доступные слои:", layers)
+
+                        layers = gpd.list_layers(file_name_and_path_bottom_layer)
+                        print("Доступные слои:", layers)
+                    else:
+                        message = f'Error when getting difference list of features.  For top layer: {top_layer_dif_info['message']}; For bottom layer: {bottom_layer_dif_info['message']}'
                 elif last_saved_version_top_layer < latest_version_top_layer['version']:
                     self.__get_difference_between_versions(self.top_layer_id, last_saved_version_top_layer, latest_version_top_layer['version'], last_saved_epoch_top_layer)
                     self.__save_file_with_cur_versions()
@@ -224,26 +258,16 @@ class LinkParametersHolder:
                 else:
                     if __debug__:
                         print('From last upd nothing was changed')
-                    return {'status':'ok'}
-
-                if __debug__:
-                    print('From last upd nothing was changed')
-                return {'status':'ok'}
-            
+                    return {'status':'ok'}    
             else:
                 message = f'Error when getting last saved version of layers. For top layer: {top_layer_info['message']}; For bottom layer: {bottom_layer_info['message']}'
-                if __debug__:
-                    print(message)
-                return {'status':'error', 'message':message}
         else:
             message = f'Error when getting version of layers. For top layer: {latest_version_top_layer['message']}; For bottom layer: {latest_version_bottom_layer['message']}'
-            if __debug__:
-                print(message)
-            return {'status':'error', 'message':message}
+        return LinkParametersHolder.__handle_error(message)
 
     def __get_latest_version_and_epoch(self, layer_id):
         """
-        This function returns the last version and epoch of the layer
+        This function returns a dict with the last version and epoch of the layer
 
 
         Parametrs
@@ -260,21 +284,14 @@ class LinkParametersHolder:
         layer_info = requests.get(req, auth = (self.ngw_login, self.ngw_password))
 
         if layer_info.status_code == 200:
+            # it would probably be better to check if these keys exist
             versioning_info = layer_info.json()['feature_layer']['versioning']
-
             versioning_status = versioning_info['enabled']
-            if not versioning_status:
-                message = f'Versioning for layer with id {layer_id} is turned off'
-                if __debug__:
-                    print(message)
-                return {'status':'error', 'message':message}
-            
-            return {'status':'ok', 'version': versioning_info['latest'], 'epoch': versioning_info['epoch']}
+            if not versioning_status: message = f'Versioning for layer with id {layer_id} is turned off'
+            else: return {'status':'ok', 'version': versioning_info['latest'], 'epoch': versioning_info['epoch']}
         else:
             message = f'Request error when getting version and epoch for the layer with id {layer_id} from the server: {layer_info.status_code}'
-            if __debug__:
-                print(message)
-            return {'status':'error', 'message':message}
+        return LinkParametersHolder.__handle_error(message)
     
     def __get_difference_between_versions(self, layer_id, previous_version, latest_version, epoch):
         """
@@ -294,8 +311,13 @@ class LinkParametersHolder:
 
         epoch : int
             current epoch of the layer
+
+        Returns
+        -------
+        dict
+            status key contains error or ok, if error then message key contains explanations, if ok then dif_list key contains the list of updated features
         """
-        req = f'{self.ngw_host}/api/resource/{layer_id}/feature/changes/check?epoch={epoch}&initial={previous_version}&target={latest_version}'
+        req = f'{self.ngw_host}/api/resource/{layer_id}/feature/changes/check?epoch={epoch}&initial={previous_version}&target={latest_version}&geom_format=geojson'
         difference_versions_info = requests.get(req, auth = (self.ngw_login, self.ngw_password))
         
         if difference_versions_info.status_code == 200:
@@ -304,24 +326,29 @@ class LinkParametersHolder:
                 print(f'Request link for layer with id {layer_id} between versions {previous_version} and {latest_version}: {req}')
                 print(f'Link for more information: {fetch}')
             result = requests.get(fetch, auth = (self.ngw_login, self.ngw_password))
-            json_result = result.json()
+            if result.status_code == 200:
+                json_result = [item for item in result.json() if "vid" in item] 
 
-            time_for_versions = self.__get_versions_information(layer_id, previous_version, latest_version)
-            for item in json_result:
-                if 'vid' in item:
-                    for time in time_for_versions:
-                        if 'id' in time:
-                            if time['id'] == item['vid']:
-                                item['time'] = time['tstamp']
-                                print("NEW VIEW", item)
-            print("UPD JSON:", json_result)
-                            
-            return {'status':'ok', 'dif_list':result.json()}
-        else:
-            message = f"Error during get the list of features for the layer {layer_id} between versions {previous_version} and {latest_version} for epoch {epoch}"
-            if __debug__:
-                print(message)
-            return {'status':'error', 'message':message}
+                requests_for_versions = self.__get_versions_information(layer_id, previous_version, latest_version)
+                if requests_for_versions['status'] == 'ok':
+                    time_for_versions = requests_for_versions['answer']
+                    for item in json_result:
+                        if 'vid' in item:
+                            for time in time_for_versions:
+                                if 'id' in time:
+                                    if time['id'] == item['vid']:
+                                        item['time'] = time['tstamp']
+                                        item['layer_id'] = layer_id
+
+                    sorted_by_time_json = sorted(json_result, key=LinkParametersHolder.__get_time)
+                    if __debug__:
+                        print("UPD JSON WITH TIME:", sorted_by_time_json)
+                                    
+                    return {'status':'ok', 'dif_list':sorted_by_time_json}
+                else: message = requests_for_versions['message']
+            else: message = f"Error when getting details about changes for the layer {layer_id} between versions {previous_version} and {latest_version} for epoch {epoch}"
+        else: message = f"Error when getting the list of features for the layer {layer_id} between versions {previous_version} and {latest_version} for epoch {epoch}"
+        return LinkParametersHolder.__handle_error(message)
 
     def __get_versions_information(self, layer_id, last_version, latest_version): # DO THERE!!!!!!!!
         """
@@ -340,7 +367,7 @@ class LinkParametersHolder:
             the last uploaded version of the layer to the cloud
         """
         answer = []
-        for version in range(last_version, latest_version):
+        for version in range(last_version, latest_version+1):
             req = f'{self.ngw_host}/api/resource/{layer_id}/feature/version/{version}'
             print('Request link to get version info: ', req)
             current_version_info = requests.get(req, auth = (self.ngw_login, self.ngw_password))
@@ -349,7 +376,7 @@ class LinkParametersHolder:
             else:
                 if __debug__:
                     print(f"Error while getting feature of version {version} for the layer with id {layer_id}")
-        return {'status':'ok', 'info':answer}
+        return {'status':'ok', 'answer':answer}
 
     def __get_last_saved_version_and_epoch_by_id(self, layer_id):
         """
@@ -376,19 +403,24 @@ class LinkParametersHolder:
                     return {'status':'ok', 'version':layer['version'], 'epoch':layer['epoch']}
 
             message = f'Error when finding local info for the layer with id: {layer_id}'
-            if __debug__:
-                print(message)
-            return {'status':'error', 'message':message}
         except FileNotFoundError:
             message = f"Error: File '{data_file_name_and_path}' not found."
         except json.JSONDecodeError:
             message = f"Error: File '{data_file_name_and_path}' contains invalid JSON."
         except Exception as e:
             message = f"Error when opening the file '{data_file_name_and_path}': {e}"
+        
+        return LinkParametersHolder.__handle_error(message)
 
+    def __handle_error(message):
         if __debug__:
             print(message)
         return {'status':'error', 'message':message}
+
+    def __get_time(item):
+        if 'time' in item:
+            return datetime.fromisoformat(item['time'])
+        else: return datetime.min
 
 def main(config_path):
     new_lph = LinkParametersHolder(config_path)
@@ -401,11 +433,3 @@ if __name__ == '__main__':
         main('config.json')
     except ErrorConnection as e:
         print(e)
-
-
-
-
-def get_layer_info(ngw_host, ngw_login, ngw_password, layer_id):
-    req = ngw_host + '/api/resource/' + str(layer_id)
-    layer_info = requests.get(req, auth = (ngw_login, ngw_password))
-    print(layer_info.json())
